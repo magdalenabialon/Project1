1fd583e8af0c34e759894ae2935604793007900b
Magdalena Bialon <magdalenabialon@gmail.com>
2016-07-13 11:59:07 +1000

    beggining: whose turn x or o

--- script.js
+++ script.js
@@ -64,15 +64,45 @@ console.log(getWinner(player1,player2));
 
 //                    ****  DOM board  ****
 
+  var whoseTurn = 'x';
+
 $('.boxes').on('click', buttonClicked);
 
-function buttonClicked(event) {
-  console.log($('.boxes')[1]);
-  console.log(event.target.id);
-  board[event.target.id] = "x"
-  console.log(board);
-}
 
+function buttonClicked(event) {
+
+  console.log(event.target.id);
+  //board[event.target.id] = 'o'; // >>> !!! variable
+
+
+  var index = event.target.id;
+  console.log(index)
+  console.log(typeof(index));
+  console.log(event.target.id);
+
+  // var whoseTurn;
+  console.log('whose turn', whoseTurn);
+
+
+
+  if(whoseTurn === 'x'){
+    console.log('x turn');
+    //board[event.target.id]
+    // console.log('whose turn', whoseTurn);
+    board[index] = 'x';
+    //indexPlayer1.push(index);
+     whoseTurn = 'o';
+//    console.log('x is at: ' + index);
+  } else {
+    console.log('y turn');
+    board[event.target.id]
+    whoseTurn = 'x';
+    board[index] = 'o';
+    //board.indexPlayer2.push(index);
+//    console.log('o is at: ' + index);
+  }
+  console.log(board)
+}
 
 
 
@@ -81,7 +111,7 @@ function buttonClicked(event) {
 
 // var scoreX = 0;       // ?? later on ??
 // var scoreO = 0;
-// var counter = 0;
+// var turn = 0;
 // var rounds = 0;
 
 
@@ -98,23 +128,25 @@ var indexPlayer2 = 4;           // player 2 each choice  -> connect with dom - h
 
 
 
-var choosePosition = function (index,player) {
+// var choosePosition = function (index,player) {
+//
+//   var arrayPlayer = [];                                        //store players choices
+//
+//     for (var round = 0; round < board.length; round++) {
+//       board[index] = player;                                   //set my board position
+//         if (board[index] === 'x') {
+//           arrayPlayer.push(index);
+//           console.log(arrayPlayer);
+//           console.log('x is at: ' + index);                             //push the choice to the  arrayPlayer
+//         } else if (board[index] === 'o') {
+//           arrayPlayer.push(index);
+//           console.log(arrayPlayer);
+//           console.log('y is at: ' + index);
+//         }
+//     }
+// }
 
-  var arrayPlayer = [];                                        //store players choices
-
-    for (var round = 0; round < board.length; round++) {
-      board[index] = player;                                   //set my board position
-        if (board[index] === 'x') {
-          arrayPlayer.push(index);
-          console.log(arrayPlayer);                             //push the choice to the  arrayPlayer
-        } else if (board[index] === 'o') {
-          arrayPlayer.push(index);
-          console.log(arrayPlayer);
-        }
-    }
-}
-
-choosePosition(indexPlayer1, player1);
-choosePosition(indexPlayer2, player2);
+// choosePosition(indexPlayer1, player1);
+// choosePosition(indexPlayer2, player2);
 
 console.log(board);
